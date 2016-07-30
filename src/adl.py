d458d352ce67047d8cf93705f71a692f5542e2d2
Arkadiy Drovosekov <arkdro@gmail.com>
2016-07-30 19:20:03 +0300

    change style

--- src/adl.py
+++ src/adl.py
@@ -1,36 +1,44 @@
-import sys
+# import sys
+
 import re
 import argparse
 
+
+def write_item(item):
+    """ write item to stdout """
+    print(item)
+
+
+def flush_item(item, include, exclude):
+    """ flush item """
+    if not item:
+        return
+    if include:
+        for i in include:
+            if (re.search(i, item, flags=re.MULTILINE | re.IGNORECASE)):
+                write_item(item)
+                break
+        return
+    elif exclude:
+        for e in exclude:
+            if (re.search(e, item, flags=re.MULTILINE | re.IGNORECASE)):
+                return
+        write_item(item)
+
+
 def proc_file(file, include, exclude):
+    """ process file """
     regex = re.compile('\d+-\d+-\d+ \d+:\d+:\d+ =\w+ REPORT====')
     item = ''
     with open(file, encoding='utf-8') as fd:
         for l in fd:
-            if(re.search(regex, l)):
+            if (re.search(regex, l)):
                 flush_item(item, include, exclude)
                 item = l
             else:
                 item += l
     flush_item(item, include, exclude)
 
-def flush_item(item, include, exclude):
-    if not item:
-        return
-    if include:
-        for i in include:
-            if(re.search(i, item, flags = re.MULTILINE | re.IGNORECASE)):
-                write_item(item)
-                break
-        return
-    elif exclude:
-        for e in exclude:
-            if(re.search(e, item, flags = re.MULTILINE | re.IGNORECASE)):
-                return
-        write_item(item)
-
-def write_item(item):
-    print(item)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
